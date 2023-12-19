854a673ed735213cb88ef086d9d87ffc620a906a
veymarmc <veymar.montano@gmail.com>
2023-12-19 04:39:50 -0400

    continue with the 5 level kata, structure

--- 5 level exercize/3FoodProteinCarbo.js
+++ 5 level exercize/3FoodProteinCarbo.js
@@ -3,5 +3,19 @@
  * @param {[string]} arr Array of food information.
  */
 function bulk(arr) {
+  function getProteinsCalories(item, [pr, car, fat]) {
+    const prTotal = calculate(item, pr);
+    const carTotal = calculate(item, car);
+    const fatTotal = calculate(item, fat);
+    return [prTotal, (prTotal + carTotal)*4 + (fatTotal*9)]
+  }
 
+  const answer = arr.reduce((res, meal, i) => {
+    const compound = meal.split(' ');
+    
+
+
+  }, [0, 0]);
+
+  return `Total protein: ${answer[0]} grams, Total calories: ${answer[1]}`
 }
