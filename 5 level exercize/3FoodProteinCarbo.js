9206fff2a8e8e99e6c8ade99e63a3e075a1efb14
veymarmc <veymar.montano@gmail.com>
2023-12-20 05:16:09 -0400

    finish with the 5 level exercise

--- 5 level exercize/3FoodProteinCarbo.js
+++ 5 level exercize/3FoodProteinCarbo.js
@@ -3,19 +3,23 @@
  * @param {[string]} arr Array of food information.
  */
 function bulk(arr) {
-  function getProteinsCalories(item, [pr, car, fat]) {
-    const prTotal = calculate(item, pr);
-    const carTotal = calculate(item, car);
-    const fatTotal = calculate(item, fat);
-    return [prTotal, (prTotal + carTotal)*4 + (fatTotal*9)]
+  function calculate(item, elem) {
+    return ((item*elem)/100).toFixed(2);
   }
 
-  const answer = arr.reduce((res, meal, i) => {
+  function getProteinsCalories(item, [pr, carb, fat]) {
+    const prTotal = calculate(item, pr);
+    const carbTotal = calculate(item, carb);
+    const fatTotal = calculate(item, fat);
+    return [+prTotal, (+prTotal + +carbTotal)*4 + (+fatTotal*9)]
+  }
+
+  // the flatMap function won't work in ancient versions of node. An alternative is to spread all internal arrays manually
+  const answer = arr.flatMap(mealChain => mealChain.split(', ')).reduce((res, meal) => {
     const compound = meal.split(' ');
-    
-
-
+    const prCalPerCompound = getProteinsCalories(+compound[0].substring(0, compound[0].length -1), food[compound[1]]);
+    return [res[0]+prCalPerCompound[0], res[1]+prCalPerCompound[1]];
   }, [0, 0]);
 
-  return `Total protein: ${answer[0]} grams, Total calories: ${answer[1]}`
+  return `Total protein: ${parseFloat(answer[0])} grams, Total calories: ${parseFloat(answer[1])}`
 }
