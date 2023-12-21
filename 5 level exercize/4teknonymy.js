7e18405ba8237f03d8ca0e21b8e23987e64789df
veymarmc <veymar.montano@gmail.com>
2023-12-21 06:50:53 -0400

    make first analysis

---
+++ 5 level exercize/4teknonymy.js
@@ -0,0 +1,7 @@
+/**
+ * https://www.codewars.com/kata/65781071e16df9dcbded1520/train/javascript
+ * @param {object} familyTree a node of a member family with references to other nodes that are it children.
+ */
+function teknonymize(familyTree){
+  // your code here
+}
