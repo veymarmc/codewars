629a04d3b2d44cc0be7f712bb2d1218b623b0a43
veymarmc <veymar.montano@gmail.com>
2024-01-02 04:27:24 -0400

    make first analysis

---
+++ 5 level exercize/5HelpYourGranny.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/5536a85b6ed4ee5a78000035/train/javascript
+ * @param {[string]} friends List of friends
+ * @param {[[string]]} fTowns Associative array that connect friends and towns.
+ * @param {Array} distTable Associative array taht connect an friend and a distance respect the granny.
+ * @returns Total distance of the visiting all friends circuit.
+ */
+function tour(friends, fTowns, distTable) {
+  return -1
+}
