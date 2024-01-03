459fce54c7613e19b8e00c4fbb335b7e8ec19492
veymarmc <veymar.montano@gmail.com>
2024-01-03 05:31:32 -0400

    finish helpGranny exercise

--- 5 level exercize/5HelpYourGranny.js
+++ 5 level exercize/5HelpYourGranny.js
@@ -2,9 +2,28 @@
  * https://www.codewars.com/kata/5536a85b6ed4ee5a78000035/train/javascript
  * @param {[string]} friends List of friends
  * @param {[[string]]} fTowns Associative array that connect friends and towns.
- * @param {Array} distTable Associative array taht connect an friend and a distance respect the granny.
+ * @param {[string, number]} distTable Associative array taht connect an friend and a distance respect the granny.
  * @returns Total distance of the visiting all friends circuit.
  */
 function tour(friends, fTowns, distTable) {
-  return -1
+  let knownDistance = 0;
+  const totalDistanceCircuit = friends.reduce((distance, friend) => {
+    const townFriend = fTowns.find((townFriend) => townFriend.includes(friend));
+    if (!townFriend)
+      return distance;
+    const townDistance = distTable.find((_, i) => townFriend[1] === distTable[i-1]);
+    if (!townDistance)
+      return distance;
+    const unknownDistance = Math.sqrt(townDistance * townDistance - knownDistance * knownDistance);
+    knownDistance = townDistance;
+    return distance + unknownDistance;
+  }, 0) + knownDistance;
+
+  return Math.floor(totalDistanceCircuit);
 }
+
+var friends1 = ["A1", "A2", "A3", "A4", "A5"];
+var fTowns1 = [["A1", "X1"], ["A2", "X2"], ["A3", "X3"], ["A4", "X4"]];
+var distTable1 = ["X1", 100.0, "X2", 200.0, "X3", 250.0, "X4", 300.0];
+
+console.log(tour(friends1, fTowns1, distTable1));
