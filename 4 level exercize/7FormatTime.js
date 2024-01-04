cd5be9a75b7d142e19640757666f5703852284fe
veymarmc <veymar.montano@gmail.com>
2024-01-04 04:01:44 -0400

    make first analysis

---
+++ 4 level exercize/7FormatTime.js
@@ -0,0 +1,8 @@
+/**
+ * https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
+ * @param {number} seconds positive integer including zero;
+ * @returns formated time in human friendly written way.
+ */
+function formatDuration (seconds) {
+  return 'now';
+}
