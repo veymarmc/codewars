1e33808aec86f5e51f3c2c4682986f4fd16d0334
veymarmc <veymar.montano@gmail.com>
2024-01-06 09:52:23 -0400

    add codewars link to the exercise

--- 3 level exercize/2HuffmanEncoding.js
+++ 3 level exercize/2HuffmanEncoding.js
@@ -1,3 +1,7 @@
+/**
+ * https://www.codewars.com/kata/54cf7f926b85dcc4e2000d9d/train/javascript;
+ * @param {string} s sequences of letters.
+ */
 // takes: String; returns: [ [String,Int] ] (Strings in return value are single characters)
 function frequencies(s) {
   
