f9b568e2861fa601011e535858e40b7e872d975e
veymarmc <veymar.montano@gmail.com>
2024-01-06 09:49:50 -0400

    make the first problem analysis

---
+++ 3 level exercize/2HuffmanEncoding.js
@@ -0,0 +1,25 @@
+// takes: String; returns: [ [String,Int] ] (Strings in return value are single characters)
+function frequencies(s) {
+  
+}
+
+// takes: [ [String,Int] ], String; returns: String (with "0" and "1")
+function encode(freqs,s) {
+  
+}
+
+// takes [ [String, Int] ], String (with "0" and "1"); returns: String
+function decode(freqs,bits) {
+  
+}
+
+/*
+Tasks to do:
+  - [o] make the frequencies algorithm given any text.
+  - [o] study the examples of the tests to know exactly how to encode and decode.
+  - [o] if the precious information is not enough, study the Huffman Encodding in order to check more examples.
+  - [o] with the result of the precious information recheck and study binary tree data structure to solve the problem.
+  - [o] make the encode algorithm
+  - [o] make the decode algorithm
+  - [o] study a little bit the tests in order to begin with unit tests.
+*/
