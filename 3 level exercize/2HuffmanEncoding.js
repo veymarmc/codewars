70a9036c0f2006ee77f16f327d0175156efadc50
veymarmc <veymar.montano@gmail.com>
2024-01-08 08:46:27 -0400

    study the basics of the huffman algorithm

--- 3 level exercize/2HuffmanEncoding.js
+++ 3 level exercize/2HuffmanEncoding.js
@@ -1,5 +1,6 @@
 /**
  * https://www.codewars.com/kata/54cf7f926b85dcc4e2000d9d/train/javascript;
+ * https://en.wikipedia.org/wiki/File:Huffman_coding_visualisation.svg
  * @param {string} s sequences of letters.
  */
 // takes: String; returns: [ [String,Int] ] (Strings in return value are single characters)
@@ -21,7 +22,7 @@ function decode(freqs,bits) {
 Tasks to do:
   - [o] make the frequencies algorithm given any text.
   - [o] study the examples of the tests to know exactly how to encode and decode.
-  - [o] if the precious information is not enough, study the Huffman Encodding in order to check more examples.
+  - [x] if the previous information is not enough, study the Huffman Encodding in order to check more examples.
   - [o] with the result of the precious information recheck and study binary tree data structure to solve the problem.
   - [o] make the encode algorithm
   - [o] make the decode algorithm
