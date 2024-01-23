29a7c260bcec1e51d4ba61789ccf99dcf7a37119
veymarmc <veymar.montano@gmail.com>
2024-01-23 05:26:08 -0400

    find most of the algorightms to use

--- 3 level exercize/2HuffmanEncoding.js
+++ 3 level exercize/2HuffmanEncoding.js
@@ -1,5 +1,5 @@
 /**
- * https://www.codewars.com/kata/54cf7f926b85dcc4e2000d9d/train/javascript;
+ * https://www.codewars.com/kata/54cf7f926b85dcc4e2000d9d/train/javascript
  * https://en.wikipedia.org/wiki/File:Huffman_coding_visualisation.svg
  * @param {string} s sequences of letters.
  */
@@ -21,9 +21,51 @@ function decode(freqs,bits) {
 /*
 Tasks to do:
   - [o] make the frequencies algorithm given any text.
-  - [o] study the examples of the tests to know exactly how to encode and decode.
+  - [x] study the examples of the tests to know exactly how to encode and decode.
   - [x] if the previous information is not enough, study the Huffman Encodding in order to check more examples.
-  - [o] with the result of the precious information recheck and study binary tree data structure to solve the problem.
+    - Data: the given string
+    - needed structures:
+      - An array of trees
+        - these tress at the beginning are only a node with weight of each letter
+        - the weight of each letter is the frequency of it in the given string.
+        - these should be ordered according to the weights.
+      - the tree has the following components:
+        - a letter (could be null if this is a branch node)
+        - the weight
+        - the children if any (exactly two since this is a binary tree, or can be null).
+        - the value of binary child, if left child this is zero, otherwise one.
+      - the decoded array has the following components:
+        - the letter
+        - the respective code
+          - this code is gotten by:
+          - going through the root to the leaf which contain the letter.
+          - take all the code values of the nodes (left: 0, right: 1)
+          - as the root node doesn't have another sibling it doesn't have a code value.
+    - the algorithm to encode:
+      - get the frequencies of each letter of the given string.
+      - put each of them in a tree with the value of the children to null.
+      - (*) order the trees acording to the root weight.
+      - take the two less weighed trees and join then into a new tree.
+        - order the two children the smaller to the left and the bigger to the right.
+        - all left children should have a zero value of code and the left ones: one.
+        - The new root should have the summation of its children weights as its weight.
+        - The new root doesn't have a letter since this is a branch node.
+      - delete the previous selected trees.
+      - put the new tree in the tree array.
+      - ask if the tree array length is 1
+        - if is greater go to (*)
+        - if so return the array of trees.
+      - decode each letter:
+        - this code is gotten by:
+        - going through the root to the leaf which contain the letter.
+        - take all the code values of the nodes (left: 0, right: 1)
+        - as the root node doesn't have another sibling it doesn't have a code value.
+      - replace each code instead of each letter of the given string.
+
+    - the algorithm to decode:
+      - have the hauffman tree
+      - traverse each bit to the hauffman tree and find the respective letter.
+  - [o] with the result of the previous information recheck and study binary tree data structure to solve the problem.
   - [o] make the encode algorithm
   - [o] make the decode algorithm
   - [o] study a little bit the tests in order to begin with unit tests.
