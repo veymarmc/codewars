e7078806ce58fc409d0036fe03349d7745145558
veymarmc <veymar.montano@gmail.com>
2024-01-25 09:44:42 -0400

    refactor private methods

--- 3 level exercize/2huffmanEncoding/2HuffmanEncoding.js
+++ 3 level exercize/2huffmanEncoding/2HuffmanEncoding.js
@@ -32,22 +32,16 @@ function buildHuffmanTree(freqs) {
 	return treeArray[0];
 }
 
-function getHuffmanCode(letter, huffmanTree, code = "") {
-	if (letter === huffmanTree.letter) return code;
-
-	return huffmanTree.children[0].letter.includes(letter)
-		? getHuffmanCode(letter, huffmanTree.children[0], code + "0")
-		: getHuffmanCode(letter, huffmanTree.children[1], code + "1");
-}
-
-function getHuffmanLetter(huffmanTree, bits) {
-	if (huffmanTree.letter.length === 1) return huffmanTree.letter;
-
-	return getHuffmanLetter(huffmanTree.children[+bits.splice(0, 1)], bits);
-}
-
 // takes: [ [String,Int] ], String; returns: String (with "0" and "1")
 function encode(freqs, s) {
+  function getHuffmanCode(letter, huffmanTree, code = "") {
+    if (letter === huffmanTree.letter) return code;
+  
+    return huffmanTree.children[0].letter.includes(letter)
+      ? getHuffmanCode(letter, huffmanTree.children[0], code + "0")
+      : getHuffmanCode(letter, huffmanTree.children[1], code + "1");
+  }
+
 	const huffmanTree = buildHuffmanTree(freqs);
 	return freqs.reduce(
 		(code, [letter]) => code.replaceAll(letter, getHuffmanCode(letter, huffmanTree)),
@@ -57,6 +51,12 @@ function encode(freqs, s) {
 
 // takes [ [String, Int] ], String (with "0" and "1"); returns: String
 function decode(freqs, bits) {
+  function getHuffmanLetter(huffmanTree, bits) {
+    if (huffmanTree.letter.length === 1) return huffmanTree.letter;
+  
+    return getHuffmanLetter(huffmanTree.children[+bits.splice(0, 1)], bits);
+  }
+
 	const huffmanTree = buildHuffmanTree(freqs);
 	const bitsArray = bits.split("");
 	let msg = "";
@@ -74,9 +74,9 @@ const s = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED";
 const freqs = frequencies(s);
 console.log(freqs);
 const huffmanTree = buildHuffmanTree(freqs);
-// console.log(JSON.stringify(huffmanTree, null, " "));
-// const huffmanCodeForA = getHuffmanCode('A', huffmanTree);
-// console.log('huffman code for A: ', huffmanCodeForA);
+console.log(JSON.stringify(huffmanTree, null, " "));
+const huffmanCodeForA = getHuffmanCode('A', huffmanTree);
+console.log('huffman code for A: ', huffmanCodeForA);
 const bits =
 	"1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001";
 console.log(encode(freqs, s) === bits);
