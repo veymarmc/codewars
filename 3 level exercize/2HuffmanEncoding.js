4dfca4a5ae3b090fb5f0974d3038a9f90e65a580
veymarmc <veymar.montano@gmail.com>
2024-01-25 09:27:07 -0400

    implement the decode algorithm

--- 3 level exercize/2HuffmanEncoding.js
+++ 3 level exercize/2HuffmanEncoding.js
@@ -23,15 +23,16 @@ function buildHuffmanTree(freqs) {
 		treeArray.sort((a, b) => a.weight - b.weight);
 		const treesToCompare = treeArray.splice(0, 2);
 		treeArray.push({
-      letter: treesToCompare[0].letter + treesToCompare[1].letter,
-      weight: treesToCompare[0].weight + treesToCompare[1].weight,
-      children: treesToCompare,
-    });
+			letter: treesToCompare[0].letter + treesToCompare[1].letter,
+			weight: treesToCompare[0].weight + treesToCompare[1].weight,
+			children: treesToCompare
+		});
 	}
 
 	return treeArray[0];
 }
 // ################ TEST PART ##################
+// ### SHOULD BE IMPLEMENTED WITH UNITESTS INTO A UNITEST FRAMEWORK
 const s = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED";
 const freqs = frequencies(s);
 console.log(freqs);
@@ -39,26 +40,46 @@ const huffmanTree = buildHuffmanTree(freqs);
 // console.log(JSON.stringify(huffmanTree, null, " "));
 // const huffmanCodeForA = getHuffmanCode('A', huffmanTree);
 // console.log('huffman code for A: ', huffmanCodeForA);
-console.log(encode(freqs, s) === "1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001");
+const bits =
+	"1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001";
+console.log(encode(freqs, s) === bits);
+console.log(decode(freqs, bits));
 
 function getHuffmanCode(letter, huffmanTree, code = "") {
-  if (letter === huffmanTree.letter)
-    return code;
+	if (letter === huffmanTree.letter) return code;
 
-  return huffmanTree.children[0].letter.includes(letter)
-    ? getHuffmanCode(letter, huffmanTree.children[0], code + '0')
-    : getHuffmanCode(letter, huffmanTree.children[1], code + '1');
+	return huffmanTree.children[0].letter.includes(letter)
+		? getHuffmanCode(letter, huffmanTree.children[0], code + "0")
+		: getHuffmanCode(letter, huffmanTree.children[1], code + "1");
+}
+
+function getHuffmanLetter(huffmanTree, bits) {
+	if (huffmanTree.letter.length === 1) return huffmanTree.letter;
+
+	return getHuffmanLetter(huffmanTree.children[+bits.splice(0, 1)], bits);
 }
 
 // takes: [ [String,Int] ], String; returns: String (with "0" and "1")
 function encode(freqs, s) {
-  const huffmanTree = buildHuffmanTree(freqs);
-  const letterCodesArray = freqs.map(([letter]) => [letter, getHuffmanCode(letter, huffmanTree)]);
-  return letterCodesArray.reduce((code, letterCode) => code.replaceAll(letterCode[0], letterCode[1]), s);
+	const huffmanTree = buildHuffmanTree(freqs);
+	return freqs.reduce(
+		(code, [letter]) => code.replaceAll(letter, getHuffmanCode(letter, huffmanTree)),
+		s
+	);
 }
 
 // takes [ [String, Int] ], String (with "0" and "1"); returns: String
-function decode(freqs, bits) {}
+function decode(freqs, bits) {
+	const huffmanTree = buildHuffmanTree(freqs);
+	const bitsArray = bits.split("");
+	let msg = "";
+
+	while (bitsArray.length > 0) {
+		msg += getHuffmanLetter(huffmanTree, bitsArray);
+	}
+
+	return msg;
+}
 
 /*
 Tasks to do:
@@ -108,9 +129,8 @@ Tasks to do:
       - have the hauffman tree
       - traverse each bit to the hauffman tree and find the respective letter.
   - [x] with the result of the previous information recheck and study binary tree data structure to solve the problem.
-  - [0] make the huffman tree construction algorithm.
-  - [o] make the encode algorithm
-  - [o] make the decode algorithm
-  - [o] study a little bit the tests in order to begin with unit tests.
+  - [x] make the huffman tree construction algorithm.
+  - [x] make the encode algorithm
+  - [x] make the decode algorithm
+  - [x] study a little bit the tests in order to begin with unit tests.
 */
-
