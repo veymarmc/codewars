36269680e919f02ca9263f25ce4c2d88ff684a1f
veymarmc <veymar.montano@gmail.com>
2024-05-04 15:07:05 -0400

    refactor and set test-debug environment

--- 3 level exercize/3MakeASpiral/3MakeASpiral.js
+++ 3 level exercize/3MakeASpiral/3MakeASpiral.js
@@ -8,41 +8,43 @@ function spiralize(n) {
 	const spiral = Array.from({ length: n }).map(_ => Array(n).fill(0));
 	// possitions to traverse through the matrix. The first represents the horizontal, the second the vertical
 	const position = [0, 0];
-	// state of the movement, the first the orientation reference, the second the orientation values vary from 0, 1 (horizontal or vertical); the third vary from 1, -1 (direction)
+	// state of the movement,
+	// the first the orientation orientation values vary from 0, 1 (horizontal or vertical),
+	// the second vary from 1, -1 (direction)
+	// the thrid the frequency of change of the direction which is 2, first 2 positives ver/horiz and then 2 negatives ver/horiz to complete a cycle.
 	let [orientation, direction, changeDirectionCounter] = [0, 1, 2];
 	let strikesLimit = 0;
 
-	debugger;
-	while (strikesLimit !== 2) {
-		debugger;
-		// const nextPosition = spiral[!orientation ? position[1] : position[1] + direction][!orientation ? position[0] + direction : position[0]];
+	while (strikesLimit !== 2) {		
 		const isInMatrixLimits = position[orientation] >= 0 && position[orientation] >= (n - 1);
+		const nextPosition = spiral[!orientation ? position[1] : position[1] + direction][!orientation ? position[0] + direction : position[0]];
 
-		debugger;
 		if (
-			isOutMatrixLimits || 
-			spiral[!orientation ? position[1] : position[1] + direction][!orientation ? position[0] + direction : position[0]] === 1
-			) {
+			!isInMatrixLimits || 
+			nextPosition === 1
+		) {
+			// giving one step back			
 			position[orientation] = position[orientation] + direction * -1;
 
+			// verify to change direction
 			if (--changeDirectionCounter == 0) {
 				direction *= -1;
 				changeDirectionCounter = 2;
 			}
 
+			// change orientation
 			orientation = 1 - orientation;
+			// positioning the next value
 			position[orientation] += direction;
+			// a limit have been reached.
 			strikesLimit++;
-			debugger;
 		} else {
 			strikesLimit = 0;
 			spiral[position[1]][position[0]] = 1;
 			position[orientation] += direction;
-			debugger;
 		}
 	}
 
-	debugger;
 	return spiral;
 }
 
