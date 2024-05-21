aeddf83e71bb6c9045088ea543b0c37b512242bc
veymarmc <veymar.montano@gmail.com>
2024-05-21 04:56:49 -0400

    init exercise

---
+++ 7 level exercize/1RepetedSequenceLength/findREpeatedSequenceLength.test.js
@@ -0,0 +1,7 @@
+const findRepeatedSequenceLength = require('./findRepeatedSequenceLength');
+
+describe('Find the repeated sequence length:', () => {
+  test('dumb test', () => {
+    expect(findRepeatedSequenceLength(0)).toEqual(0);
+  })
+});
---
+++ 7 level exercize/1RepetedSequenceLength/findRepeatedSequenceLength.js
@@ -0,0 +1,12 @@
+/**
+ * https://www.codewars.com/kata/58a3f57ecebc06bfcb00009c/train/javascript
+ * @param {number} a0 the number to begin the sequence, positive Integer greater than zero
+ * @returns the size of the final repeated sequence.
+ */
+function findRepeatedSequenceLen(a0) {
+  //coding and coding..
+  
+  return 0;
+}
+
+module.exports = findRepeatedSequenceLen;
