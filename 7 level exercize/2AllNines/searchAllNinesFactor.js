cb065ae37e733d35b0ad2301868ea7dab2aedbea
veymarmc <veymar.montano@gmail.com>
2024-05-22 16:54:00 -0400

    implement params edge cases result

--- 7 level exercize/2AllNines/searchAllNinesFactor.js
+++ 7 level exercize/2AllNines/searchAllNinesFactor.js
@@ -4,6 +4,8 @@
  * @returns big integer that multiplied by x the result is formed by only 9 for all its digits or -1 if there's no such a number.
  */
 function searchAllNinesFactor(x) {
+  if(x % 2 === 0 || x % 5 === 0) return -1;
+
   return 0n; // x is a bigint
 }
 
--- 7 level exercize/2AllNines/searchAllNinesFactor.test.js
+++ 7 level exercize/2AllNines/searchAllNinesFactor.test.js
@@ -1,8 +1,12 @@
 const searchAllNinesFactor = require("./searchAllNinesFactor");
 
 describe("Search all Nines factor", () => {
-  test("even numbers should give -1", () => {
-    const computed = searchAllNinesFactor(12);
-    expect(computed).toBe(-1);
-  })
+  test("Special numbers edge cases should give -1", () => {
+    // even numbers
+    let evaluatedNumber = 12;
+    expect(searchAllNinesFactor(evaluatedNumber)).toBe(-1);
+    // multiple of 5 numbers
+    evaluatedNumber = 135;
+    expect(searchAllNinesFactor(135)).toBe(-1);
+  });
 });
