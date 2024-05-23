8a1d8a4ae808590c315e7467f6434c762cf989e0
veymarmc <veymar.montano@gmail.com>
2024-05-23 07:53:04 -0400

    add more tests and delete changes in package.json

--- 7 level exercize/2AllNines/searchAllNinesFactor.test.js
+++ 7 level exercize/2AllNines/searchAllNinesFactor.test.js
@@ -1,4 +1,16 @@
 const searchAllNinesFactor = require("./searchAllNinesFactor");
+let multipleTests = [
+  [11n, 9n],
+  [12n, -1n],
+  [7n, 142857n],
+  [9n, 1n],
+  [1n, 9n],
+  [3n, 3n],
+  [31n, 32258064516129n],
+  [63n, 15873n],
+  [189n, 5291n],
+  [65n, -1n]
+];
 
 describe("Search all Nines factor", () => {
   test("Special numbers edge cases should give -1", () => {
@@ -10,6 +22,7 @@ describe("Search all Nines factor", () => {
     evaluatedNumber = 135n;
     expect(searchAllNinesFactor(evaluatedNumber)).toBe(expected);
   });
+  
   test("Basic numbers, known cases", () => {
     let expected = 9n;
     let actual = searchAllNinesFactor(11n);
@@ -19,5 +32,9 @@ describe("Search all Nines factor", () => {
     actual = searchAllNinesFactor(13n);
     expect(actual).toBe(expected);
   });
+  
+  // other multiple known tests.
+  test.each(multipleTests)('Test the PARAM x=%i and EXPECTED m=%i', (param, expected) => {
+    expect (searchAllNinesFactor(param)).toBe(expected);
+  })
 });
-
