cdca8992a54fb194adfd309b3427c791af405841
veymarmc <veymar.montano@gmail.com>
2024-05-23 07:07:16 -0400

    implement first solution

--- 7 level exercize/2AllNines/searchAllNinesFactor.js
+++ 7 level exercize/2AllNines/searchAllNinesFactor.js
@@ -1,12 +1,18 @@
 /**
  * https://www.codewars.com/kata/664b9dd610985cc3b6784111/train/javascript
- * @param {number} x 
+ * @param {BigInt} x 
  * @returns big integer that multiplied by x the result is formed by only 9 for all its digits or -1 if there's no such a number.
  */
 function searchAllNinesFactor(x) {
-  if(x % 2 === 0 || x % 5 === 0) return -1;
+  if(x % 2n === 0n || x % 5n === 0n) return -1n;
 
-  return 0n; // x is a bigint
+  let ninesNumber = BigInt([...`${x}`].map(_ => '9').join(''));
+
+  while (ninesNumber % x !== 0n) {
+    ninesNumber = ninesNumber * 10n + 9n;
+  }
+
+  return ninesNumber / x;
 }
 
 module.exports = searchAllNinesFactor;
--- 7 level exercize/2AllNines/searchAllNinesFactor.test.js
+++ 7 level exercize/2AllNines/searchAllNinesFactor.test.js
@@ -2,11 +2,22 @@ const searchAllNinesFactor = require("./searchAllNinesFactor");
 
 describe("Search all Nines factor", () => {
   test("Special numbers edge cases should give -1", () => {
+    const expected = -1n;
     // even numbers
-    let evaluatedNumber = 12;
-    expect(searchAllNinesFactor(evaluatedNumber)).toBe(-1);
+    let evaluatedNumber = 12n;
+    expect(searchAllNinesFactor(evaluatedNumber)).toBe(expected);
     // multiple of 5 numbers
-    evaluatedNumber = 135;
-    expect(searchAllNinesFactor(135)).toBe(-1);
+    evaluatedNumber = 135n;
+    expect(searchAllNinesFactor(evaluatedNumber)).toBe(expected);
+  });
+  test("Basic numbers, known cases", () => {
+    let expected = 9n;
+    let actual = searchAllNinesFactor(11n);
+    expect(actual).toBe(expected);
+
+    expected = 76923n;
+    actual = searchAllNinesFactor(13n);
+    expect(actual).toBe(expected);
   });
 });
+
