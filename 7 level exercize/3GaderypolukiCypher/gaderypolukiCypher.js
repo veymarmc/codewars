9f63cf422c15c3ae21820adc3987f1687d769876
veymarmc <veymar.montano@gmail.com>
2024-05-24 07:33:22 -0400

    init exercise

---
+++ 7 level exercize/3GaderypolukiCypher/gaderypolukiCypher.js
@@ -0,0 +1,14 @@
+/**
+ * https://www.codewars.com/kata/592a6ad46d6c5a62b600003f/train/javascript
+ * @param {*} str 
+ * @returns 
+ */
+function encode(str) 
+{
+    return str;   
+}
+
+function decode(str) 
+{
+    return str;   
+}
