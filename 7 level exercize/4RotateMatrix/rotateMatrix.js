a4484389c786e75fb56d49d04e34c38cc4f97abd
veymarmc <veymar.montano@gmail.com>
2024-05-25 06:25:11 -0400

    init exercise

---
+++ 7 level exercize/4RotateMatrix/rotateMatrix.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/56b5dd1702a30326ce000b02/train/javascript
+ * @param {Array} arr Matrix to rotate
+ * @return the rotated matrix
+ */
+function rotateMatrix(arr) {
+  return arr;
+}
+
+module.exports = rotateMatrix;
