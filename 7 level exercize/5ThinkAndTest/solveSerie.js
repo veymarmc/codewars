4876b2ac16794974470e7808b2836e41dd8e9a1a
veymarmc <veymar.montano@gmail.com>
2024-06-01 16:44:20 -0400

    init exercise

---
+++ 7 level exercize/5ThinkAndTest/solveSerie.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/javascript
+ * @param {number} a positive integer
+ * @param {number} b positive integer
+ * @returns the next element of a series
+ */
+function testit(a,b){
+  //return a+b ?
+  //return a*b ?
+  return a + b || a * b;
+}
