7f12fa73935cc86dc7eba4003ddccc30b0fd30c9
veymarmc <veymar.montano@gmail.com>
2024-06-03 06:27:09 -0400

    init exercise

---
+++ 7 level exercize/6BritishSlovakianPostcodes/decipherWhichPostcode.js
@@ -0,0 +1,8 @@
+/**
+ * https://www.codewars.com/kata/664e4f5b12b1b20182ea3e3a/train/javascript
+ * @param {string} postcode 
+ * @returns the current postcode name of the given code
+ */
+function decipherWhichPostcode(postcode){
+  return "";
+}
