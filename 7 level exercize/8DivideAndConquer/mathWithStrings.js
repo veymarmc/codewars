8d60efe64c46393dee6b0280f3d2e87555646e3e
veymarmc <veymar.montano@gmail.com>
2024-06-05 05:52:50 -0400

    init exercise

---
+++ 7 level exercize/8DivideAndConquer/mathWithStrings.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/57eaec5608fed543d6000021/train/javascript
+ * @param {[number, string]} x 
+ * @returns the sum of all numbers minus the sum of all string integers
+ */
+function extractValueFromArray(x){
+  return 0;
+}
+
+module.exports = extractValueFromArray;
