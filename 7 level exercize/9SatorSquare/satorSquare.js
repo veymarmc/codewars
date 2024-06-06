50afc715e7fab0b1607ff74c72eccf4861c0ea1a
veymarmc <veymar.montano@gmail.com>
2024-06-06 08:08:48 -0400

    init exercise

---
+++ 7 level exercize/9SatorSquare/satorSquare.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/5cb7baa989b1c50014a53333/train/javascript
+ * @param {Array[string]} tablet possible sator square
+ * @returns if the input is a sator square
+ */
+function isSatorSquare(tablet) {
+  return false;
+}
+
+module.exports = isSatorSquare;
