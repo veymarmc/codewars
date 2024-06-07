d181e5b93c16eecb8e376cbda63877f3019b5077
veymarmc <veymar.montano@gmail.com>
2024-06-07 07:41:02 -0400

    implement other propopsal better solutions

--- 7 level exercize/10MergeOvelapingStrings/mergeOverlapingStrings.js
+++ 7 level exercize/10MergeOvelapingStrings/mergeOverlapingStrings.js
@@ -4,7 +4,7 @@
  * @param {string} second 
  * @returns The merged string which take on account the overlapping section to merge.
  */
-function mergeOverlappingStrings(first, second){
+function mergeOverlappingStrings1(first, second){
   for (let i = 0; i < first.length; i++) {
     const overlap = first.slice(i);
     
@@ -13,6 +13,19 @@ function mergeOverlappingStrings(first, second){
   }
 
   return first + second;
+}
+
+// best solution
+function mergeOverlappingStrings2(first, second) {
+  for (let i = Math.min(first.length, second.length); i >= 0; i--) {
+    if(second.startsWith(first.slice(first.length - i)))
+      return first + second.slice(i);
+  }
+}
+
+// clever solution
+function mergeOverlappingStrings(first, second) {
+  return (first + ' ' + second).replace(/(.*) \1/, '$1');
 }
 
 module.exports = mergeOverlappingStrings;
