0f9663844e31e4f20b30056afbc0999911b10d2c
veymarmc <veymar.montano@gmail.com>
2024-06-07 06:06:49 -0400

    init exercise

---
+++ 7 level exercize/10MergeOvelapingStrings/mergeOverlapingStrings.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/61c78b57ee4be50035d28d42/train/javascript
+ * @param {string} first 
+ * @param {string} second 
+ * @returns The merged string which take on account the overlapping section to merge.
+ */
+function mergeOverlappingStrings(first, second){
+  return first + second;
+}
+
+module.exports = mergeOverlappingStrings;
