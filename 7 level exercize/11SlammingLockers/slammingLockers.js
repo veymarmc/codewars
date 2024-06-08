bcdf4b13959401288ad66ed4f834449693255e27
veymarmc <veymar.montano@gmail.com>
2024-06-08 07:17:29 -0400

    init exercise

---
+++ 7 level exercize/11SlammingLockers/slammingLockers.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/55a5085c1a3d379fbb000062/train/javascript
+ * @param {n} lockers number of lockers
+ * @returns an array of open lockers after slamming.
+ */
+function findOpenLockers(lockers) {
+  return [lockers];
+}
+
+module.exports = findOpenLockers;
