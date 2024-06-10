5ffae23c6b6ac70fd5b5ec525d22e6d1c2ebc137
veymarmc <veymar.montano@gmail.com>
2024-06-10 10:06:22 -0400

    init exercise

---
+++ 6 level exercize/1RobotTransfer/robotTransfer.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/58aaa3ca821a767300000017/train/javascript
+ * @param {Array} matrix Set of coordinates to go
+ * @param {number} k number of steps to take on account.
+ * @returns all the round-trips that number of steps are equal to k
+ */
+function robotTransfer(matrix, k) {
+  return 0
+}
+
+module.exports = robotTransfer;
