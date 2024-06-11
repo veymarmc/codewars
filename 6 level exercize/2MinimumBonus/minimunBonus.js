aa0d8abe7458a7d2c51a1895220d796fb6ea46d2
veymarmc <veymar.montano@gmail.com>
2024-06-11 09:17:18 -0400

    init exercise

---
+++ 6 level exercize/2MinimumBonus/minimunBonus.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/595ef0c7458ad5facc000019/train/javascript
+ * @param {Array} scores teams' scores
+ * @returns the total bonus given
+ */
+function getMinimumBonus(scores){
+  return 0;
+}
+
+module.exports = getMinimumBonus;
