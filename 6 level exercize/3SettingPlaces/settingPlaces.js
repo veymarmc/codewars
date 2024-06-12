dfbb5fa4705af5b11d541fb5bf433e0e16a79593
veymarmc <veymar.montano@gmail.com>
2024-06-12 07:16:19 -0400

    init exercise

---
+++ 6 level exercize/3SettingPlaces/settingPlaces.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/6646c0c08b97085ca216d346/train/javascript
+ * @param {Array} theDead array of names to be place at the table
+ * @returns The table seats arranged
+ */
+function setTable(theDead) {
+  return [];
+}
+
+module.exports = setTable;
