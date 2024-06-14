4aac5f44876bf978256900fe9dd4adda0fe4fbed
veymarmc <veymar.montano@gmail.com>
2024-06-14 05:38:45 -0400

    init exercise

---
+++ 6 level exercize/4NextNumberBinary.js/nextNumberBinary.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.codewars.com/kata/56bdd0aec5dc03d7780010a5/train/javascript
+ * @param {number} n integer between 1 and 1<<30
+ * @returns the next higher number with same 1 digists.
+ */
+function getHigherWithSame1Digits(n) {
+  return 0
+}
+
+module.exports = getHigherWithSame1Digits;
