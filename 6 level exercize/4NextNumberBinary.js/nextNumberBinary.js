cd12c540b920a3b5d7e42c59b8f2eac5e60475a8
veymarmc <veymar.montano@gmail.com>
2024-06-14 09:44:46 -0400

    add a better solution found

--- 6 level exercize/4NextNumberBinary.js/nextNumberBinary.js
+++ 6 level exercize/4NextNumberBinary.js/nextNumberBinary.js
@@ -3,13 +3,18 @@
  * @param {number} n integer between 1 and 1<<30
  * @returns the next higher number with same 1 digists.
  */
-function getNextNumberSame1Digits(n) {
+function getNextNumberSame1Digits2(n) {
 	const binary = n.toString(2);
 	const patternIndex = binary.lastIndexOf('01');
 
 	return patternIndex !== -1
     ? parseInt(binary.replace(/(.*)(01)(1*)(0*)$/, '$110$4$3'), 2)
     : parseInt(binary.replace(/(1)(1*)(0*)/, '$1$30$2'), 2);
+}
+
+function getNextNumberSame1Digits(n) {
+  let s = n.toString(2).replace(/0?1(1*)(0*)$/, "10$2$1"); // best pattern found!
+  return parseInt(s,2);
 }
 
 module.exports = getNextNumberSame1Digits;
