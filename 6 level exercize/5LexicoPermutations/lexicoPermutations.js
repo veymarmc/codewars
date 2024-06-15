6b542415a898c7b5d7bc0870ec73dba6c487a871
veymarmc <veymar.montano@gmail.com>
2024-06-15 08:53:28 -0400

    init exercise

---
+++ 6 level exercize/5LexicoPermutations/lexicoPermutations.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/55baa55cf332e67eb000000a/train/javascript
+ * @param {number} n the position of the lexico word
+ * @param {number} d the number of digist to use from 0
+ * @return The nth lexicographic permuted word
+ */
+function getNthLexicoPermutation(n,d) {
+  return ''
+}
+
+module.exports = getNthLexicoPermutation;
