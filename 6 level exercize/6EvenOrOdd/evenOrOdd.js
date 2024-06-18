bc7a67b93aa596c029161b946a8329221be85211
veymarmc <veymar.montano@gmail.com>
2024-06-18 04:27:12 -0400

    init exercise

---
+++ 6 level exercize/6EvenOrOdd/evenOrOdd.js
@@ -0,0 +1,8 @@
+/**
+ * https://www.codewars.com/kata/6656a4687f3e4eb5fb520817/train/javascript
+ * @param {number} n integer to check if even or odd
+ * @returns return "even" or "odd"
+ */
+function isEvenOrOdd(n) {
+  return '';
+}
