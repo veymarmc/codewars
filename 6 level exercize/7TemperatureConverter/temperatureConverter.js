eaacb3d1fe7fa834e98acfd02b30967afcb4ac8f
veymarmc <veymar.montano@gmail.com>
2024-06-19 05:09:25 -0400

    init exercise

---
+++ 6 level exercize/7TemperatureConverter/temperatureConverter.js
@@ -0,0 +1,12 @@
+/**
+ * https://www.codewars.com/kata/54ce9497975ca65e1a0008c6/train/javascript
+ * @param {number} temp integer, the temperature to convert
+ * @param {string} from_scale the current scale
+ * @param {string} to_scale the scale to convert
+ * @returns an integer represented the converted temperature
+ */
+function convertTemp(temp, from_scale, to_scale){
+  return 0;
+}
+
+module.exports = convertTemp;
