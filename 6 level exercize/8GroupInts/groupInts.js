52d0a7ca86773cfd38aacda7601778536ab5bc94
veymarmc <veymar.montano@gmail.com>
2024-06-21 05:03:42 -0400

    init exercise

---
+++ 6 level exercize/8GroupInts/groupInts.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/583fe48ca20cfc3a230009a1/train/javascript
+ * @param {Array} xs list of integers
+ * @param {number} x interger key to get nested lists
+ * @returns An array of ordered arrays according to the key.
+ */
+function groupInts(xs,x) {
+  return [];
+}
+
+module.exports = groupInts;
