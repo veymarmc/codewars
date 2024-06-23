96e1d8a8797ff5bbde069a1bb698404461762632
veymarmc <veymar.montano@gmail.com>
2024-06-23 04:02:56 -0400

    init exercise

---
+++ 6 level exercize/9onlyDuplicates/onlyDuplicates.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/5a1dc4baffe75f270200006b/train/javascript
+ * @param {string} str 
+ * @returns the string with only duplicate characters
+ */
+function keepOnlyDuplicates(str) {
+  // your code here
+  return str
+}
+
+module.exports = keepOnlyDuplicates;
