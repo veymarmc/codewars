24c2a160649d807d802f8877580e7b4fa12a013e
veymarmc <veymar.montano@gmail.com>
2024-06-25 05:24:00 -0400

    init exercise

---
+++ 6 level exercize/10boundingBox/boundingBox.js
@@ -0,0 +1,8 @@
+/**
+ * https://www.codewars.com/kata/6644683b7c83db9ba33b7021/train/javascript
+ * @param {Array} imageArray 
+ * @returns an Array with the bounding box of the image.
+ */
+function boundingBox(imageArray) {
+  return [];
+}
