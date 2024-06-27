fef1711226c7840e622787b8ec92c2987edcc7a6
veymarmc <46348503+veymarmc@users.noreply.github.com>
2024-06-27 04:01:38 -0400

    Bounding Box (#27)
    * init exercise
    
    * add basic tests
    
    * implement solution
    
    * correct solution to work with mxn matrix
    
    * refine solution

---
+++ 6 level exercize/10boundingBox/boundingBox.js
@@ -0,0 +1,38 @@
+/**
+ * https://www.codewars.com/kata/6644683b7c83db9ba33b7021/train/javascript
+ * @param {Array} imageArray
+ * @returns an Array with the bounding box of the image.
+ */
+function getBoundingBox(imageArray) {
+	let [x1, y1, x2, y2] = [];
+
+	for (let y = 0; y < imageArray.length; y++) {
+		for (let x = 0; x < imageArray[0].length; x++) {
+			if (imageArray[y][x]) {
+				x1 = x > x1 ? x1 : x;
+				y1 = y > y1 ? y1 : y;
+				x2 = x < x2 ? x2 : x;
+				y2 = y < y2 ? y2 : y;
+			}
+
+			imageArray[y][x] = 0;
+		}
+	}
+
+	if (x1 < 0) return imageArray;
+
+	for (let i = x1; i <= x2; i++) {
+		imageArray[y1][i] = 1;
+		imageArray[y2][i] = 1;
+	}
+
+	for (let i = y1; i <= y2; i++) {
+		imageArray[i][x1] = 1;
+		imageArray[i][x2] = 1;
+	}
+
+	return imageArray;
+}
+
+module.exports = getBoundingBox;
+
