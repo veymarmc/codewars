cc2ba7df96c7d09151f08c9b6ecdb835342b347e
veymarmc <veymar.montano@gmail.com>
2024-06-28 04:52:10 -0400

    init exercise

---
+++ 6 level exercize/11CardsPlay/cardsPlay.js
@@ -0,0 +1,9 @@
+/**
+ * https://www.codewars.com/kata/53417de006654f4171000587/train/javascript
+ * @param {Array} deckSteve cards of Steve
+ * @param {Array} deckJosh cards of Josh
+ * @returns Who wins message and score, or "Tie"
+ */
+function getWinnerOfDecks(deckSteve, deckJosh) {
+  return "";
+}
