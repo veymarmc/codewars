e467e13d3f0bfc03b55d2ad0208671a5aee06fa1
veymarmc <veymar.montano@gmail.com>
2024-06-28 05:25:57 -0400

    implement solution

--- 6 level exercize/11CardsPlay/cardsPlay.js
+++ 6 level exercize/11CardsPlay/cardsPlay.js
@@ -1,3 +1,5 @@
+const highCards = { T: 10, J: 11, Q: 12, K: 13, A: 14 };
+
 /**
  * https://www.codewars.com/kata/53417de006654f4171000587/train/javascript
  * @param {Array} deckSteve cards of Steve
@@ -5,5 +7,21 @@
  * @returns Who wins message and score, or "Tie"
  */
 function getWinnerOfDecks(deckSteve, deckJosh) {
-  return "";
+	let steveScore = 0;
+	let joshScore = 0;
+	const getCardValue = card => highCards[card] ?? +card;
+
+	for (let i = 0; i < deckSteve.length; i++) {
+		const steveCard = getCardValue(deckSteve[i]);
+		const joshCard = getCardValue(deckJosh[i]);
+
+		if (steveCard !== joshCard)
+      steveCard > joshCard ? steveScore++ : joshScore++;
+	}
+
+	if (steveScore === joshScore) return 'Tie';
+
+	const winner = steveScore > joshScore ? 'Steve' : 'Josh';
+
+	return `${winner} wins ${Math.max(steveScore, joshScore)} to ${Math.min(steveScore, joshScore)}`;
 }
