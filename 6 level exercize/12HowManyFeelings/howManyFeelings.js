329e9e0bbce44271ed480c10f4a1e36294b00f8f
veymarmc <veymar.montano@gmail.com>
2024-06-30 11:31:35 -0400

    init exercise

---
+++ 6 level exercize/12HowManyFeelings/howManyFeelings.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/5a33ec23ee1aaebecf000130/train/javascript
+ * @param {String} string chaing where you can found feelings
+ * @param {Array} array array of feelings(written words)
+ * @returns the number the feelings found in "1 feeling" format.
+ */
+function countFeelings(string, array) {
+  return "";
+}
+
+module.exports = countFeelings;
---
+++ 6 level exercize/12HowManyFeelings/howManyFeelings.test.js
@@ -0,0 +1,8 @@
+const countFeelings = require('./howManyFeelings');
+
+describe("Basic tests",() => {
+  expect(countFeelings('yliausoenvjw', ['anger', 'awe', 'joy', 'love', 'grief'])).toBe('3 feelings.');
+  expect(countFeelings('angerw', ['anger', 'awe', 'joy', 'love', 'grief'])).toBe('2 feelings.');
+  expect(countFeelings('griefgriefgrief', ['anger', 'awe', 'joy', 'love', 'grief'])).toBe('1 feeling.');
+  expect(countFeelings('abcdkasdfvkadf', ['desire', 'joy', 'shame', 'longing', 'fear'])).toBe('0 feelings.');
+});
