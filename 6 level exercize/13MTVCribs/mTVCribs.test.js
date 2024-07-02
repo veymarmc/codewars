7329ceee3db3e91bbf7d03ca544979bae18c4de2
veymarmc <veymar.montano@gmail.com>
2024-07-02 04:30:38 -0400

    implement solution

--- 6 level exercize/13MTVCribs/mTVCribs.test.js
+++ 6 level exercize/13MTVCribs/mTVCribs.test.js
@@ -2,12 +2,12 @@ const buildCrib = require('./mTVCribs');
 
 describe("Testing", function() {
   test("Let's check your mansion", () => {
-    console.log('Your house:\n' + myCrib(1), '\nHow it should look:\n /\\ \n/__\\\n|__|');
-    expect(myCrib(1)).toBe( 
+    console.log('Your house:\n' + buildCrib(1), '\nHow it should look:\n /\\ \n/__\\\n|__|');
+    expect(buildCrib(1)).toBe( 
     ' /\\ \n/__\\\n|__|');
-    console.log('Your house:\n ' + myCrib(2), '\nHow it should look:\n  /\\  \n /  \\ \n/____\\\n|    |\n|____|');
-    expect(myCrib(2)).toBe('  /\\  \n /  \\ \n/____\\\n|    |\n|____|');
-    console.log('Your house:\n' + myCrib(3), '\nHow it should look:\n   /\\   \n  /  \\  \n /    \\ \n/______\\\n|      |\n|      |\n|______|');
-    expect(myCrib(3)).toBe('   /\\   \n  /  \\  \n /    \\ \n/______\\\n|      |\n|      |\n|______|');
+    console.log('Your house:\n ' + buildCrib(2), '\nHow it should look:\n  /\\  \n /  \\ \n/____\\\n|    |\n|____|');
+    expect(buildCrib(2)).toBe('  /\\  \n /  \\ \n/____\\\n|    |\n|____|');
+    console.log('Your house:\n' + buildCrib(3), '\nHow it should look:\n   /\\   \n  /  \\  \n /    \\ \n/______\\\n|      |\n|      |\n|______|');
+    expect(buildCrib(3)).toBe('   /\\   \n  /  \\  \n /    \\ \n/______\\\n|      |\n|      |\n|______|');
   });
 });
