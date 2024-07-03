67b6744cb9d6d45ba61392611fd436dda7d08412
veymarmc <veymar.montano@gmail.com>
2024-07-03 07:39:45 -0400

    init exercise

---
+++ 6 level exercize/14EnglishBeggars/englishBeggars.js
@@ -0,0 +1,9 @@
+/**
+ * https://www.codewars.com/kata/59590976838112bfea0000fa/train/javascript
+ * @param {Array} values array of numbers to assing to beggars
+ * @param {number} n number of beggars to distribute
+ * @returns an array that shows how much each beggar have after distribution
+ */
+function distributeTobeggars(values, n){
+  return [];
+}
