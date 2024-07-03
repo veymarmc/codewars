7e51d208509d64738fc726dad29a70c47a919b98
veymarmc <veymar.montano@gmail.com>
2024-07-03 08:29:37 -0400

    implement solution

--- 6 level exercize/14EnglishBeggars/englishBeggars.test.js
+++ 6 level exercize/14EnglishBeggars/englishBeggars.test.js
@@ -2,10 +2,10 @@ const distributeTobeggars = require('./englishBeggars');
 
 describe("Distribute to beggars",() =>{
   test('Basic tests', () => {
-    expect(distributeTobeggars([1,2,3,4,5],1)).toBe([15]);
-    expect(distributeTobeggars([1,2,3,4,5],2)).toBe([9,6]);
-    expect(distributeTobeggars([1,2,3,4,5],3)).toBe([5,7,3]);
-    expect(distributeTobeggars([1,2,3,4,5],6)).toBe([1,2,3,4,5,0]);
-    expect(distributeTobeggars([1,2,3,4,5],0)).toBe([]);
+    expect(distributeTobeggars([1,2,3,4,5],1)).toEqual([15]);
+    expect(distributeTobeggars([1,2,3,4,5],2)).toEqual([9,6]);
+    expect(distributeTobeggars([1,2,3,4,5],3)).toEqual([5,7,3]);
+    expect(distributeTobeggars([1,2,3,4,5],6)).toEqual([1,2,3,4,5,0]);
+    expect(distributeTobeggars([1,2,3,4,5],0)).toEqual([]);
   });
 });
